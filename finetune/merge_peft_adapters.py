273ec257279ebbb011a44ffb804cf294c9da07ef
Pan, Yujie <yujie.pan@intel.com>
2023-08-03 12:47:58 +0800

    modify lora merge

--- finetune/merge_peft_adapters.py
+++ finetune/merge_peft_adapters.py
@@ -1,6 +1,7 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from peft import PeftModel
 import torch
+from pathlib import Path
 
 import os
 import argparse
@@ -16,25 +17,30 @@ def get_args():
 def main():
     args = get_args()
 
+    print('Loading model...')
     base_model = AutoModelForCausalLM.from_pretrained(
         args.base_model_name_or_path,
         return_dict=True,
         torch_dtype=torch.float16 
     )
 
+    print('Merging model...')
     model = PeftModel.from_pretrained(base_model, args.peft_model_path)
     model = model.merge_and_unload()
 
-    tokenizer = AutoTokenizer.from_pretrained(args.base_model_name_or_path)
+    tokenizer = AutoTokenizer.from_pretrained(args.base_model_name_or_path, trust_remote_code=True)
 
-    if args.push_to_hub:
+    if args.push_to_hub and False:  # never upload to hf
         print(f"Saving to hub ...")
         model.push_to_hub(f"{args.base_model_name_or_path}-merged", use_temp_dir=False, private=True)
         tokenizer.push_to_hub(f"{args.base_model_name_or_path}-merged", use_temp_dir=False, private=True)
     else:
-        model.save_pretrained(f"{args.base_model_name_or_path}-merged")
-        tokenizer.save_pretrained(f"{args.base_model_name_or_path}-merged")
-        print(f"Model saved to {args.base_model_name_or_path}-merged")
+        print('Saving model...')
+        save_path = Path(args.peft_model_path).parent / (Path(args.peft_model_path).name + "-merged")
+        save_path = save_path.expanduser().resolve().as_posix()
+        model.save_pretrained(save_path)
+        tokenizer.save_pretrained(save_path)
+        print(f"Model saved to {save_path}")
 
 if __name__ == "__main__" :
     main()
